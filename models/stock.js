55013187a5d41740b6340cee202ccb785519a173
Karimyounes <your-email@example.com>
2025-11-10 14:02:36 +0100

    changer style

--- models/stock.js
+++ models/stock.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const stockSchema = new mongoose.Schema({
   designation: { type: String, required: true },
   qte:         { type: Number, required: true },
-  ns:          { type: String, required: true },
  
 });
 
