dfae46ad76818b7a56ea931f37e5cae84603d838
Karimyounes <your-email@example.com>
2025-12-07 21:27:37 +0100

    Fix merge conflicts - keep local APC sections

--- models/sectionmodel.js
+++ models/sectionmodel.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 
 const mongoose = require('mongoose');
 
 
 
-=======
-
-const mongoose = require('mongoose');
->>>>>>> 02b1afd0d98877df3dc0f0ca9c45feab5667d737
 const sectionSchema = new mongoose.Schema({
   nom: { type: String, required: true },
   chef: { type: String, required: true },
